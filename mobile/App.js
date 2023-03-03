d70abe20ee6e684c02b85d66d6876b8f286b96c6
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-03-03 01:56:43 -0800

    sign in with google attempt

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
+      qsd
     </View>
   );
 }
